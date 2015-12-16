776d9b37a906021dcaef68ef1183c7ae3ccf3d4a
Alexcyt <ictcyt@163.com>
2015-12-16 10:45:51 +0800

    静态页面初步设计

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "淘书网"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} - #{page_title}"
+    end
+  end
+
 end
