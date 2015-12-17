331367b84c835d73e15484086c0f630738a0fda1
Alexcyt <ictcyt@163.com>
2015-12-17 15:48:29 +0800

    注册功能实现

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "欢迎您使用淘书网!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_conformation)
+    end
 end
